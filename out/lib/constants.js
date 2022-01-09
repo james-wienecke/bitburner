eee295471fd6f73f6135eb94cc62c57272f58a3d
jwin00 <soder96@gmail.com>
2022-01-09 08:55:39 -0600

    another library for getting game constants

---
+++ out/lib/constants.js
@@ -0,0 +1,12 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+
+}
+
+/**
+ * 
+ * @returns {number} - max ram possible for a purchased server
+ */
+function serverMaxRam() {
+    return 1048576;
+}
