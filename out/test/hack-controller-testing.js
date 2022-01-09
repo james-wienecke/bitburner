674bb81bca10455033acd9b9d1b564b5e06a4ee1
james-wienecke <james.a.wienecke@gmail.com>
2022-01-09 13:18:54 -0600

    Setting up basic object structures for important files.

--- out/test/hack-controller-testing.js
+++ out/test/hack-controller-testing.js
@@ -1,4 +1,20 @@
 // this will be the main weaken/grow/hack controller
 export async function main(ns) {
-    
+	const flags = ns.flags([
+		['log', true],         // should default to false for production code, enables verbose logging
+        ['delay', 1000],        // specifies delay for primary loop
+        ['norepeat', false],    // ??? probably will not stay in to production lol
+	]);
+
+    const script = {}
+    script.dir = "/test/";
+    script.path = script.dir + "hack-controller-testing.js"
+
+    const atk = {
+        weak:   script.dir + "atk/weaken.js",
+        grow:   script.dir + "atk/grow.js",
+        hack:   script.dir + "atk/hack.js",
+    }
+
+
 }
