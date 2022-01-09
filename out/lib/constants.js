9631346e023bcc38361c2bc949591101b1dcf810
jwin00 <soder96@gmail.com>
2022-01-09 09:58:28 -0600

    Moving more ram-eating default ns functions to constants lib.

--- out/lib/constants.js
+++ out/lib/constants.js
@@ -4,8 +4,15 @@ export async function main(ns) {
 }
 
 /**
- * Returns the maximum ram allowed for a purchased server
+ * Returns the maximum ram allowed for a purchased server.
  */
 export function serverMaxRam() {
     return 1048576;
+}
+
+/**
+ * Returns the maximum number of purchased servers the player can have.
+ */
+export function purchaseServerLimit() {
+    return 25;
 }
