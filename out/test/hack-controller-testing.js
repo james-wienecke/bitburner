2e681ff6063b3f04ffc04ccb51004541fe2aae20
james-wienecke <james.a.wienecke@gmail.com>
2022-01-09 13:59:23 -0600

    Trying to start building a breadth-first search of all connections. Will probably move it to a library.

--- out/test/hack-controller-testing.js
+++ out/test/hack-controller-testing.js
@@ -6,15 +6,36 @@ export async function main(ns) {
         ['norepeat', false],    // ??? probably will not stay in to production lol
 	]);
 
-    const script = {}
+    const script = {};
     script.dir = "/test/";
     script.path = script.dir + "hack-controller-testing.js"
 
-    const atk = {
-        weak:   script.dir + "atk/weaken.js",
-        grow:   script.dir + "atk/grow.js",
-        hack:   script.dir + "atk/hack.js",
+    for(const host of ns.scan().filter(server => !ns.getPurchasedServers().includes(server))) {
+        ns.tprint(`\n` +
+            `${host}\tgrow time: ${ns.getGrowTime(host).toFixed(2)}ms\n` +
+            `${host}\tweak time: ${ns.getWeakenTime(host).toFixed(2)}ms\n` +
+            `${host}\thack time: ${ns.getHackTime(host).toFixed(2)}ms\n\n`
+            );
     }
 
+    // let growTime = ns.getGrowTime("joesguns");
+    // tprint(ns.getGrowTime("joesguns"));
 
+    // atk contains the paths to the hack, grow, and weaken scripts as well as thread multipliers to control how much RAM is put into each
+    const atk = {
+        weak: {
+            path:       script.dir + "atk/weak.js",
+            tRatio:     6,
+        },
+        grow: {
+            path:       script.dir + "atk/grow.js",
+            tRatio:     22,
+        },
+        hack: {
+            path:       script.dir + "atk/hack.js",
+            tRatio:     1,
+        }
+    };
+
+    
 }
