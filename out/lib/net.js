8dcac4f7ed1fcb451ae3d49f2ece720048e83888
james-wienecke <james.a.wienecke@gmail.com>
2022-01-09 22:01:11 -0600

    Implemented a queue object. Queue should probably be moved to its own lib. Wrote a breadth-first search function.

--- out/lib/net.js
+++ out/lib/net.js
@@ -1,16 +1,40 @@
 export async function main(ns) {
 
 }
+export class Queue {
+    constructor() {
+        this.elems = [];
+    }
+}
+Queue.prototype.enQueue = function(elem) {
+    this.elems.push(elem);
+}
+Queue.prototype.deQueue = function() {
+    return this.elems.shift();
+}
+Queue.prototype.isEmpty = function() {
+    return this.elems.length === 0;
+}
+Queue.prototype.peek = function() {
+    return !this.isEmpty() ? this.elems[0] : undefined;
+}
 
-export const getConnections = (ns, {depth, root, nukeable}) => {
-    if (depth === undefined) depth = 1;
-    if (root === undefined) root = true;
-    if (nukeable === undefined) nukeable === true;
-    
-    for(const host of ns.scan().filter(server => !ns.getPurchasedServers().includes(server))) {
-        ns.scan()
-        .filter(server => !ns.getPurchasedServers().includes(server))
-        .forEach((server, index, connections) => {
+export const breadthFirstSearch = (ns, server) => {
+    const q = new Queue();
+    const explored = new Set();
+    q.enQueue(server);
+    explored.add(server);
 
-        })
+    while (!q.isEmpty()) {
+        const node = q.deQueue();
+        ns.scan(node)
+            .filter(s => !explored.has(s))
+            .forEach(s => {
+                explored.add(s);
+                q.enQueue(s);
+            });
+
+    }
+
+    return explored;
 }
