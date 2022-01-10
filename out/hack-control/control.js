d27b2e3581dc220813088448f14756756b5323d0
james-wienecke <james.a.wienecke@gmail.com>
2022-01-10 00:27:29 -0600

    Prepped control.js for non-local execution. Straightened out the uglybad flags. But they could use more love.

--- out/hack-control/control.js
+++ out/hack-control/control.js
@@ -6,12 +6,12 @@ import { dukeNukem } from "/lib/hutil.js";
  */
 export async function main(ns) {
 	const flags = ns.flags([
-		['log', true],         // should default to false for production code, enables verbose logging
-        ['delay', 12000],        // specifies delay for primary loop
-        ['norepeat', false],    // ??? probably will not stay in to production lol
+		['log', false],         // should default to false for production code, enables verbose logging
+        ['reserve', 256]
 	]);
 
-    
+    // custom logging
+    ns.disableLog('ALL');
 
     const script = {};
     script.dir = "/hack-control/";
@@ -34,7 +34,7 @@ export async function main(ns) {
     };
 
     // reserve some ram for home machine
-    const homeReserved = 256;
+    const homeReserved = flags.reserve;
 
     // get all servers in the net...
     const allServers = Array.from(breadthFirstSearch(ns, 'home'));
@@ -47,7 +47,7 @@ export async function main(ns) {
     for (let server of validServers) {
         if (!ns.hasRootAccess(server)) {
             dukeNukem(ns, server);
-            ns.print(`${server} owned`);
+            if (flags.log) ns.print(`${server} owned`);
         }
     }
 
@@ -57,11 +57,11 @@ export async function main(ns) {
             return (ns.getServerMaxMoney(current) / ns.getServerMinSecurityLevel(current) > ns.getServerMaxMoney(best) / ns.getServerMinSecurityLevel(best)) ? current : best;
         });
         
-        // ns.print(`\n` +
-        //     `${moneyOverSec}\thack lvl req: ${ns.getServerRequiredHackingLevel(moneyOverSec)}\n` +
-        //     `${moneyOverSec}\tmax $: $${ns.getServerMaxMoney(moneyOverSec).toFixed(2)}\n` +
-        //     `${moneyOverSec}\tmin sec: ${ns.getServerMinSecurityLevel(moneyOverSec).toFixed(2)}`
-        // );
+        if (flags.log) ns.print(`\n` +
+            `${moneyOverSec}\thack lvl req: ${ns.getServerRequiredHackingLevel(moneyOverSec)}\n` +
+            `${moneyOverSec}\tmax $: $${ns.getServerMaxMoney(moneyOverSec).toFixed(2)}\n` +
+            `${moneyOverSec}\tmin sec: ${ns.getServerMinSecurityLevel(moneyOverSec).toFixed(2)}`
+        );
 
         // set loop intent
         let intent = 'weak';
@@ -77,23 +77,27 @@ export async function main(ns) {
             timeout = ns.getHackTime(moneyOverSec) + 5000;
         }
 
-        ns.print('loop intent: ' + intent);
-
         // temp - will be replaced with all valid servers when loop is added
-        let host = 'home';
-        ns.print('max ram on host ' + host + ' is ' + ns.getServerMaxRam(host) + 'GB');
+        for (let host of validServers) {
+            if (flags.log) ns.print('max ram on host ' + host + ' is ' + ns.getServerMaxRam(host) + 'GB');
 
-        // calc threads available to each atk script
-        if (host === 'home') {
-            atk[intent].t = Math.floor((ns.getServerMaxRam(host) - homeReserved) / ns.getScriptRam(atk[intent].p));
-        } else {
-            atk[intent].t = Math.floor(ns.getServerMaxRam(host) / ns.getScriptRam(atk[intent].p));
+            // calc threads available to each atk script
+            if (host === 'home') {
+
+                atk[intent].t = Math.floor((ns.getServerMaxRam(host) - homeReserved) / ns.getScriptRam(atk[intent].p));
+            } else {
+                atk[intent].t = Math.floor(ns.getServerMaxRam(host) / ns.getScriptRam(atk[intent].p));
+            }
+            
+            // check if atk file already exists on host...
+            if (!ns.fileExists(atk[intent].p, host)) {
+                await ns.scp(atk[intent].p, home, host);
+            }
+
+
+            ns.print('running ' + intent + ' with ' + atk[intent].t + ' threads (' + ns.getScriptRam(atk[intent].p) * atk[intent].t + 'GB)');
+            ns.exec(atk[intent].p, host, atk[intent].t, moneyOverSec);
         }
-
-        // await ns.scp().....
-
-        ns.tprint('running ' + intent + ' with ' + atk[intent].t + ' threads (' + ns.getScriptRam(atk[intent].p) * atk[intent].t + 'GB)');
-        ns.exec(atk[intent].p, host, atk[intent].t, moneyOverSec);
 
         ns.print('waiting ' + timeout + 'ms (' + timeout / 1000 + 's)');
         await ns.sleep(timeout);
