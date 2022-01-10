931cc31a3bb511c970398de20fe31e4b5d5b1c85
james-wienecke <james.a.wienecke@gmail.com>
2022-01-09 22:02:56 -0600

    Testing Queue and breadth-first search function.

--- out/test/scratch.js
+++ out/test/scratch.js
@@ -1,28 +1,19 @@
 import * as math from "/lib/math";
+import { Queue, breadthFirstSearch } from "/lib/net";
 //import { serverMaxRam } from "/lib/constants.js";
 
 export async function main (ns) {
     ns.tprint("hello from vscode");
 
-    const testCorrectPowOfTwo = () => {
-        ns.tprint('Correcting value = 19 to closest power of 2');
-        ns.tprint(math.correctPowerOfTwo(19));
-        ns.tprint('Correcting value = 255 to closest power of 2');
-        ns.tprint(math.correctPowerOfTwo(255));
-        ns.tprint('Correcting value = 256 to closest power of 2');
-        ns.tprint(math.correctPowerOfTwo(256));
-        ns.tprint('Correcting value = 1,048,577 to closest power of 2');
-        ns.tprint(math.correctPowerOfTwo(1048577));
-        ns.tprint('Correcting value = 262,145 to closest power of 2');
-        ns.tprint(math.correctPowerOfTwo(262145));
-        ns.tprint('Correcting value = 1,048,600 to closest power of 2');
-        ns.tprint(math.correctPowerOfTwo(1048600));
-    }
-
-    // testing power of two library functions
-    testCorrectPowOfTwo();
 
     testPassNSEnv(ns, 'testing...');
+
+    const servers = breadthFirstSearch(ns, 'home');
+
+    for (let server of servers) {
+        ns.tprint(server);
+    }
+    ns.tprint('we did it, girls and gays');
 }
 
 const testPassNSEnv = (ns, str) => {
